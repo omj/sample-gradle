0d5177e44c57a0a75c20f2a2a9758b3e6b4b74ea
gene <jerihofreek@gmail.com>
2017-11-29 15:45:18 +0200

    some utils

--- src/main/java/example/HelloWorld.java
+++ src/main/java/example/HelloWorld.java
@@ -1,8 +1,12 @@
 package example;
 
+import org.joda.time.DateTime;
+
 public class HelloWorld {
  
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        java.util.Date juDate = new java.util.Date();
+        DateTime dt = new DateTime(juDate);
     }
 }
---
+++ src/main/java/example/SomeUtils.java
@@ -0,0 +1,5 @@
+package example;
+
+public class SomeUtils {
+    public static int CONSTANT = 42;
+}
